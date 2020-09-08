1c7927e15c9fef269851e1ae7fa1832644ff369d
linyuanze <1577932922@qq.com>
2020-09-08 17:19:13 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# react-native-echarts
-react-native echarts
+# native-webview-echarts 
+base on [react-native-webview](https://github.com/react-native-community/react-native-webview/) and Echarts
+
+make sure install react-native-webview first.
+
+## Getting Started
+
+### 1.install
+```
+
+yarn add native-webview-echarts 
+
+
+```
+
+### 2.other setting
+Put the files (i.e. Folder: echartsAssets) to `android/app/src/main/assets` and `/ios`
+
+Add the files to your project in Xcode (click "Add Files to 'your project'")
+
+## Usage
+
+read the [react-native-webview API Reference](https://github.com/react-native-community/react-native-webview/blob/master/docs/Reference.md)
+
+read the [Echarts Option](https://echarts.apache.org/option.html#title)
+
+
+
+
--- index.js
+++ index.js
@@ -4,14 +4,33 @@ import { WebView } from 'react-native-webview';
 
 export default class Echart extends React.Component {
 
+  componentWillReceiveProps(nextProps) {
+    if(JSON.stringify(nextProps.option) !== JSON.stringify(this.props.option)) {
+      this.webview.reload();
+    }
+  }
+
   render() {
-    const { ...rst } = this.props;
+    const { option, height, width, style, webviewStyle, ...rst } = this.props;
     const htmlUrl = Platform.OS === 'ios' ? 'echartsAssets/echart.html' : 'file:///android_asset/echartsAssets/echart.html';
     const baseUrl = Platform.OS === 'ios' ? 'echartsAssets/' : 'file:///android_asset/echartsAssets/';
     return (
       <WebView
+        ref={ref => this.webview = ref}
+        injectedJavaScript={`
+          document.getElementById('main').style.height = "${height}";
+          document.getElementById('main').style.width = "${width}";
+          var myChart = echarts.init(document.getElementById('main'));
+          myChart.setOption(${JSON.stringify(option)});
+        `}
         source={{'uri':htmlUrl,baseUrl}}
         originWhitelist={['*']}
+        style={{
+          height,
+          width,
+          backgroundColor: 'transparent',
+          ...webviewStyle
+        }}
         { ...rst }
       />
     )
